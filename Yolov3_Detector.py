5f9408597b3eb7ce2256716ae3bd43d8da77f51a
Mohamed Amine <medamine.benabdeljelil.99@gmail.com>
2023-08-26 19:25:35 +0200

    added fastapi img upload

--- Yolov3_Detector.py
+++ Yolov3_Detector.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import os
 
 class Detector:
-    resize = (320, 320)
-    def __init__(self):
+
+    def __init__(self, ):
+
         pass
     def load_model(self, config_path = './yolov3.cfg', weights_path = './yolov3.weights', verbose = True):
         # ok
@@ -30,9 +31,69 @@ class Detector:
         blob = cv2.dnn.blobFromImage(img, 1 / 255, size, [0, 0, 0], 1, crop=False)
         return blob
 
-    def set_detection_params(self, score_threshold=0.8, NMS_threshold=0.5):
+    def set_detection_params(self, score_threshold=0.5, NMS_threshold=0.5):
         self.score_threshold = score_threshold
         self.NMS_threshold = NMS_threshold
-    def run_detection_on_img(self, img, score_threshold=0.8, NMS_threshold=0.5):
 
-        pass
+
+    def run_detection_on_img(self, img):
+        blob = self.preprocess_img(img)
+        self.net.setInput(blob)
+        layersNames = self.net.getLayerNames()
+
+        # Perform forward pass through the DNN model
+        output_layers_idx = self.net.getUnconnectedOutLayers()[0] - 1
+        outputNames = [(layersNames[idx - 1]) for idx in self.net.getUnconnectedOutLayers()]
+        # print(outputNames)
+        outputs = self.net.forward(outputNames)
+        # ----------------------------------------------------------------------------------------
+        # Get the shape of the input image
+        hT, wT, cT = img.shape
+
+        # Create empty lists to store the bounding boxes, class IDs and confidence scores for detected objects
+        bbox = []
+        classIds = []
+        confs = []
+
+        # Loop over each output of the DNN model after forward pass
+        for output in outputs:
+            # Loop over each detection in the output
+            for det in output:
+                # Extract the class ID, confidence score and bounding box coordinates from the detection
+                scores = det[5:]
+                classId = np.argmax(scores)
+                confidence = scores[classId]
+                if confidence > self.score_threshold:
+                    w, h = int(det[2] * wT), int(det[3] * hT)
+                    x, y = int((det[0] * wT) - w / 2), int((det[1] * hT) - h / 2)
+                    bbox.append([x, y, w, h])
+                    classIds.append(classId)
+                    confs.append(float(confidence))
+
+        # Perform non-maximum suppression to eliminate overlapping bounding boxes
+        indices = cv2.dnn.NMSBoxes(bbox, confs, self.score_threshold, self.NMS_threshold)
+    # ----------------------------------------------------------------------------------------
+
+        """ results_dict = {
+            'bbox_xywh': [],
+            'confs': [],
+            'labels': []
+        }"""
+
+        results_list = []
+        # Loop over each index in the indices list
+        for i in indices:
+
+            result_dict = {}
+            # Get the bounding box coordinates, class label and confidence score for the current index
+            box = bbox[i]
+            class_id = classIds[i]
+            conf = int(confs[i] * 100)
+
+            result_dict['label'] = self.labels[class_id]
+            result_dict['confidence'] = conf
+            result_dict['bbox_xywh'] = box
+
+            results_list.append(result_dict)
+
+        return results_list
--- main.py
+++ main.py
@@ -1,15 +1,57 @@
-# This is a sample Python script.
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from Yolov3_Detector import Detector
+import numpy as np
+from io import BytesIO
+import cv2
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from collections import Counter
 
 
-# Press the green button in the gutter to run the script.
+def read_image(file) -> np.ndarray:
+    # Create a BytesIO stream from the uploaded file
+    image_stream = BytesIO(file)
+
+    # Move the stream's position to the beginning
+    image_stream.seek(0)
+
+    # Read the bytes from the stream and decode them using OpenCV
+    # This will decode the image data into a NumPy array
+    image = cv2.imdecode(np.frombuffer(image_stream.read(), np.uint8), cv2.IMREAD_COLOR)
+
+    return image
+
+
+class DetectionResults(BaseModel):
+    filename : str = None
+    results_str : str = 'No detections'
+    results_list : list = None
+
+
+class DetectionParams(BaseModel):
+    score_threshold: float = 0.5
+    NMS_threshold: float = 0.5
+
+app = FastAPI()
+
+detector = Detector()
+detector.load_COCO_labels()
+detector.load_model()
+
+
+@app.post("/detection/")
+async def detect_on_img(file: UploadFile = File(...) ):
+
+    results = DetectionResults()
+    img = read_image(await file.read())
+    detector.set_detection_params()
+    results.results_list = detector.run_detection_on_img(img)
+    results.filename = file.filename
+    if len(results.results_list) :
+        results.results_str = f'Found {len(results.results_list)} objects'
+    return results
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    uvicorn.run(app, host='127.0.0.1', port=8000)
